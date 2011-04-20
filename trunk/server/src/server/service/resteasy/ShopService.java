108f9497943eb3e50301cc70e868611218281825
me@majidkhosravi.com <me@majidkhosravi.com@d89037e6-9e93-2e45-5bae-03e4b61edd91>
2011-04-20 14:23:11 +0000

    AddProduct method added to the resteasy which allows adding a product by sending JSON data.

--- trunk/server/src/server/service/resteasy/ShopService.java
+++ trunk/server/src/server/service/resteasy/ShopService.java
@@ -5,8 +5,13 @@
 
 package server.service.resteasy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -14,8 +19,14 @@ import java.util.List;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import server.entitybean.Product;
 import server.entitybean.ProductImpl;
@@ -45,6 +56,41 @@ public class ShopService
 		catch (NamingException ex)
 		{
 		}
+	}
+	
+	@POST
+	@Path("/addProduct")
+	@Consumes({"application/json", "application/xml"}) 
+	public Response addProduct(InputStream is) {
+		
+		
+		Product product = readProduct(is);
+		//System.out.println(product.getName());
+		shop.addProduct(product.getName(), product.getPicture(), product.getPrice(), product.getQuantity(), product.getDescription());
+		return Response.created(URI.create("/product/" + product.getId())).build();
+	}
+	
+	private Product readProduct(InputStream is){
+		
+		Product product = null;
+		
+		String result= convertStreamToString(is);
+		 
+        // A Simple JSONObject Creation
+        JSONObject json;
+		try
+		{
+			json = new JSONObject(result);
+			product = new ProductImpl(json.getString("name"), json.getString("picture"), json.getDouble("price"), json.getInt("quantity"), json.getString("description"));
+
+		}
+		catch (JSONException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return product;
 	}
 
 	@GET
@@ -79,4 +125,31 @@ public class ShopService
 
 		return (ProductImpl) shop.findProduct(id);
 	}
+	
+	 private static String convertStreamToString(InputStream is) {
+	        /*
+	         * To convert the InputStream to String we use the BufferedReader.readLine()
+	         * method. We iterate until the BufferedReader return null which means
+	         * there's no more data to read. Each line will appended to a StringBuilder
+	         * and returned as String.
+	         */
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+	        StringBuilder sb = new StringBuilder();
+	 
+	        String line = null;
+	        try {
+	            while ((line = reader.readLine()) != null) {
+	                sb.append(line + "\n");
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                is.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        return sb.toString();
+	    }
 }
